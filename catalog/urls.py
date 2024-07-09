b71d65c29ade9dbe098d7704893dd41f06b97a09
konov <konovalova_ekaterina_a@mail.ru>
2024-07-09 20:05:04 +0300

    done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, products_list
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path("", home, name="home"), path("contacts", contacts, name="contacts")]
+urlpatterns = [path("", home, name="home"), path("contacts/", contacts, name="contacts"),
+               path("products_list/", products_list, name="products_list"),]
